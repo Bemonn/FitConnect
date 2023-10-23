7edb282e4d00acc6819f217ea240027f39ae1f3a
YJPuk <133621780+YJPuk@users.noreply.github.com>
2023-10-23 18:47:49 +1100

    added typedefs resolvers and auth, changed models to be more streamlined

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -1,30 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const appointmentSchema = new Schema({
   appointmentDate: {
-    type: Date,
+    type: String,
     required: true,
   },
   startTime: {
-    type: Date,
+    type: String,
     required: true,
   },
   endTime: {
-    type: Date,
+    type: String,
     required: true,
   },
-  trainer: {
-    type: Schema.Types.ObjectId,
-    ref: 'Trainer',
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
+  // trainer: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: 'Trainer',
+  // },
+  // user: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: 'User',
+  // },
 });
 
-const Appointment = mongoose.model('Appointment', appointmentSchema);
-
-module.exports = Appointment;
+module.exports = appointmentSchema;
