181c0e68f378610685413b2d4172f3656054978d
Hailin Ruan <hlruan829@gmail.com>
2023-10-24 20:22:52 +1100

    front end structure

---
+++ client/src/data.js
@@ -0,0 +1,60 @@
+export const trainers = [
+  {
+    title: "Trainer 1",
+    subtitle: "Weightlifting & Powerlifting",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "/trainer-1.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Trainer 2",
+    subtitle: "Nutrition & Fat Loss",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "/trainer-2.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Trainer 3",
+    subtitle: "Strength & Conditioning",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "/trainer-3.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Trainer 4",
+    subtitle: "Physio & Rehabilitation",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "/trainer-4.png",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
