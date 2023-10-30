b7938c9ac7342fb3b93388ec25e51485e3c2549b
YJPuk <133621780+YJPuk@users.noreply.github.com>
2023-10-31 01:32:13 +1100

    dashboard fixes, made to a table format, edited booking.jsx so only relevant data for sessions goes to Mongodb, display trainers names based on their numerical trainer id on dashboard page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_ME } from './../utils/queries';
+import { DELETE_APPOINTMENT } from './../utils/mutations'
+
+const trainerMapping = {
+  1: "John Lifter",
+  2: "Emily Cardio",
+  3: "Amanda Strength",
+  4: "Ben Physio",
+};
 
 const Dashboard = () => {
     const { data, loading, error } = useQuery(QUERY_ME);
+    const [deleteAppointment] = useMutation(DELETE_APPOINTMENT);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
@@ -12,22 +21,44 @@ const Dashboard = () => {
 
     return (
         <div>
-            <h1>Welcome, {firstName}!</h1>
-            <p>This is your dashboard where you can see and manage your appointments.</p>
-
-            <div>
-                <h2>Your Appointments</h2>
-                <ul>
-                    {appointments.map(appointment => (
-                        <li key={appointment._id}>
-                            <span>{appointment.selectedDate} at {appointment.selectedTime} with {appointment.trainer}</span>
-                            <button style={{color: 'red'}}>Cancel Appointment</button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+          <h1>Welcome, {firstName}!</h1>
+          <p>This is your dashboard where you can see and manage your appointments.</p>
+    
+          <div>
+            <h2>Your Appointments</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Time</th>
+                  <th>Trainer</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {appointments.map((appointment) => (
+                  <tr key={appointment._id}>
+                    <td>{appointment.selectedDate}</td>
+                    <td>{appointment.selectedTime}</td>
+                    <td>{trainerMapping[appointment.selectedTrainer]}</td>
+                    <td>
+                    <button
+                    style={{ color: 'red' }}
+                    onClick={() => {
+                        console.log("Appointment deleted")
+                        deleteAppointment({
+                        variables: { id: appointment.id },
+                      });
+                    }}
+                  >Cancel Appointment</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-    );
-}
+      );
+    };
 
 export default Dashboard;
